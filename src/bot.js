e4177c30cb100b98f8c64cd8fed28b5132a44ac3
ihorbeliasnyk <ihorbeliasnyk@gmail.com>
2021-08-17 04:41:18 +0300

    fix start webhook

--- src/bot.js
+++ src/bot.js
@@ -6,8 +6,6 @@ if (
   || !process.env.YT_API_KEY
   || !process.env.SPOTIFY_USERNAME
   || !process.env.SPOTIFY_PASSWORD
-  || !process.env.SECRET_PATH
-  || !process.env.HOST
 ) {
   throw new Error('MISSING CREDENTIALS');
 }
@@ -25,8 +23,11 @@ bot.on('message', (ctx) => ctx.reply("I'm inline bot, use me in chats with @MusV
 bot.on('inline_query', queryHandler);
 
 if (process.env.NODE_ENV === 'production') {
+  if (!process.env.SECRET_PATH || !process.env.HOST) {
+    throw new Error('MISSING WEBHOOK CONFIGS');
+  }
   bot.telegram.setWebhook(`${process.env.HOST}${process.env.SECRET_PATH}`);
-  bot.startWebhook(process.env.SECRET_PATH, null, process.env.PORT);
+  bot.startWebhook(process.env.SECRET_PATH, undefined, process.env.PORT);
 } else {
   bot.launch();
 }
